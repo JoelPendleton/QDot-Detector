8760d7e19f16e961bd328ccca994d7bd5b594812
Joel Pendleton <JoelPendleton@users.noreply.github.com>
2021-09-01 12:23:53 +0100

    Removed HRSC2016 formatted test files for my dataset as it introduced bugs

--- tools/retinanet/test_qdot.py
+++ tools/retinanet/test_qdot.py
@@ -6,47 +6,29 @@ from __future__ import division
 
 import os
 import sys
-import tensorflow as tf
-import time
-import cv2
-import pickle
-import numpy as np
-import argparse
-from tqdm import tqdm
 sys.path.append("../../")
 
 from libs.models.detectors.retinanet import build_whole_network
 from tools.test_qdot_base import TestQDOT
 from libs.configs import cfgs
-from libs.val_libs.voc_eval_r import EVAL
 
 
 class TestQDOTRetinaNet(TestQDOT):
 
     def eval(self):
+        txt_name = '{}.txt'.format(self.cfgs.VERSION)
+        real_test_img_list = self.get_test_image()
+
         retinanet = build_whole_network.DetectionNetworkRetinaNet(cfgs=self.cfgs,
                                                                   is_training=False)
+        self.test_qdot(det_net=retinanet, real_test_img_list=real_test_img_list, txt_name=txt_name)
 
-        all_boxes_r = self.eval_with_plac(img_dir=self.args.img_dir, det_net=retinanet,
-                                          image_ext=self.args.image_ext)
-
-        # with open(cfgs.VERSION + '_detections_r.pkl', 'rb') as f2:
-        #     all_boxes_r = pickle.load(f2)
-        #
-        #     print(len(all_boxes_r))
-
-        imgs = os.listdir(self.args.img_dir)
-        real_test_imgname_list = [i.split(self.args.image_ext)[0] for i in imgs]
-
-        print(10 * "**")
-        print('rotation eval:')
-        evaler = EVAL(self.cfgs)
-        evaler.voc_evaluate_detections(all_boxes=all_boxes_r,
-                                       test_imgid_list=real_test_imgname_list,
-                                       test_annotation_path=self.args.test_annotation_path)
-
+        if not self.args.show_box:
+            os.remove(txt_name)
 
 if __name__ == '__main__':
 
     tester = TestQDOTRetinaNet(cfgs)
     tester.eval()
+
+
