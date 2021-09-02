aa845411a4ed8fc9c4b86e5b84b38d56f50538ac
Joel Pendleton <JoelPendleton@users.noreply.github.com>
2021-09-02 18:56:25 +0100

    Changed config to retinanet as forgot to add images to tf record

--- libs/configs/cfgs.py
+++ libs/configs/cfgs.py
@@ -3,7 +3,7 @@ from __future__ import division, print_function, absolute_import
 
 import numpy as np
 
-from libs.configs._base_.models.faster_rcnn_r50_fpn import *
+from libs.configs._base_.models.retinanet_r50_fpn import *
 from libs.configs._base_.datasets.qdot_detection import *
 from libs.configs._base_.schedules.schedule_1x import *
 from dataloader.pretrained_weights.pretrain_zoo import PretrainModelZoo
@@ -12,7 +12,7 @@ from dataloader.pretrained_weights.pretrain_zoo import PretrainModelZoo
 BATCH_SIZE = 1
 GPU_GROUP = "0,1,2,3"
 NUM_GPU = len(GPU_GROUP.strip().split(','))
-LR = 0.001 * BATCH_SIZE * NUM_GPU
+LR = 1e-3
 SAVE_WEIGHTS_INTE = 5000 * 2
 DECAY_STEP = np.array(DECAY_EPOCH, np.int32) * SAVE_WEIGHTS_INTE
 MAX_ITERATION = SAVE_WEIGHTS_INTE * MAX_EPOCH
@@ -20,6 +20,16 @@ WARM_SETP = int(WARM_EPOCH * SAVE_WEIGHTS_INTE)
 
 # dataset
 DATASET_NAME = 'QDOT'
+IMG_SHORT_SIDE_LEN = 300
+IMG_MAX_LENGTH = 300
+CLASS_NUM = 1
+
+# data augmentation
+IMG_ROTATE = True
+RGB2GRAY = True
+VERTICAL_FLIP = True
+HORIZONTAL_FLIP = True
+IMAGE_PYRAMID = False
 
 # model
 # backbone
@@ -27,5 +37,17 @@ pretrain_zoo = PretrainModelZoo()
 PRETRAINED_CKPT = pretrain_zoo.pretrain_weight_path(NET_NAME, ROOT_PATH)
 TRAINED_CKPT = os.path.join(ROOT_PATH, 'output/trained_weights')
 
-VERSION = 'FPN_Res50D_QDOT_20210902'
+# loss
+CLS_WEIGHT = 1.0
+REG_WEIGHT = 1.0
+REG_LOSS_MODE = None
 
+VERSION = 'RetinaNet_QDOT_2x_20210902'
+
+"""
+02/09/21 at 18:41
+
+cls : diamond|| Recall: 0.7965260545905707 || Precison: 0.37066974595842955|| AP: 0.6992939356348402
+F1:0.7800504693722755 P:0.8827586206896552 R:0.6987593052109181
+mAP is : 0.6992939356348402
+"""
