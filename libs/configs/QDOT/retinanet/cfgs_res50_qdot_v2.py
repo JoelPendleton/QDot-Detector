19470a6946ec6e3771da5c285986ca35d507f3b3
Joel Pendleton <JoelPendleton@users.noreply.github.com>
2021-09-03 12:22:37 +0100

    Changed config so I use v3 retinanet

--- libs/configs/QDOT/retinanet/cfgs_res50_qdot_v2.py
+++ libs/configs/QDOT/retinanet/cfgs_res50_qdot_v2.py
@@ -36,33 +36,12 @@ VERSION = 'RetinaNet_QDOT_v2_20210903'
 RetinaNet-H + IoU-Smooth L1
 FLOPs: 484911740;    Trainable params: 33002916
 
-This is your result for task 1:
+rotation eval:
+Writing diamond VOC resutls file
+cls : diamond|| Recall: 0.7831265508684864 || Precison: 0.5125040597596622|| AP: 0.6831505284920858
+F1:0.731235047515294 P:0.7882960413080895 R:0.6818858560794044
+mAP is : 0.6831505284920858
 
-mAP: 0.6699231893137383
-ap of each class:
-plane:0.8833785173522034,
-baseball-diamond:0.7627482529936743,
-bridge:0.44320593902405797,
-ground-track-field:0.6785841556477691,
-small-vehicle:0.6303299319074853,
-large-vehicle:0.5124927246071527,
-ship:0.7277748791449373,
-tennis-court:0.8980387801428189,
-basketball-court:0.79974279949969,
-storage-tank:0.7797862635611005,
-soccer-ball-field:0.5409846307060925,
-roundabout:0.632179992142947,
-harbor:0.5621019025063557,
-swimming-pool:0.6734955940136754,
-helicopter:0.5240034764561138
-
-The submitted information is :
-
-Description: RetinaNet_DOTA_1x_20201225_45.9w
-Username: SJTU-Det
-Institute: SJTU
-Emailadress: yangxue-2019-sjtu@sjtu.edu.cn
-TeamMembers: yangxue
 """
 
 
--- libs/configs/cfgs.py
+++ libs/configs/cfgs.py
@@ -10,14 +10,12 @@ from dataloader.pretrained_weights.pretrain_zoo import PretrainModelZoo
 
 # schedule
 BATCH_SIZE = 1
-GPU_GROUP = "0,1,2,3"
+GPU_GROUP = "0"
 NUM_GPU = len(GPU_GROUP.strip().split(','))
 SAVE_WEIGHTS_INTE = 5000 * 2
 DECAY_STEP = np.array(DECAY_EPOCH, np.int32) * SAVE_WEIGHTS_INTE
 MAX_ITERATION = SAVE_WEIGHTS_INTE * MAX_EPOCH
 WARM_SETP = int(WARM_EPOCH * SAVE_WEIGHTS_INTE)
-
-# dataset
 
 # model
 # backbone
@@ -25,44 +23,46 @@ pretrain_zoo = PretrainModelZoo()
 PRETRAINED_CKPT = pretrain_zoo.pretrain_weight_path(NET_NAME, ROOT_PATH)
 TRAINED_CKPT = os.path.join(ROOT_PATH, 'output/trained_weights')
 
+# bbox head
+METHOD = 'R'
+ANCHOR_RATIOS = [1, 1 / 3., 3.]
+
 # loss
 CLS_WEIGHT = 1.0
 REG_WEIGHT = 1.0 / 5.0
-REG_LOSS_MODE = 1  # IoU-Smooth L1
+REG_LOSS_MODE = None
 
-VERSION = 'RetinaNet_QDOT_v2_20210903'
+VERSION = 'RetinaNet_QDOT_v3_20210903'
 
 """
-RetinaNet-H + IoU-Smooth L1
-FLOPs: 484911740;    Trainable params: 33002916
-
+RetinaNet-R + 90
+FLOPs: 512292465;    Trainable params: 34524216
 This is your result for task 1:
 
-mAP: 0.6699231893137383
-ap of each class:
-plane:0.8833785173522034,
-baseball-diamond:0.7627482529936743,
-bridge:0.44320593902405797,
-ground-track-field:0.6785841556477691,
-small-vehicle:0.6303299319074853,
-large-vehicle:0.5124927246071527,
-ship:0.7277748791449373,
-tennis-court:0.8980387801428189,
-basketball-court:0.79974279949969,
-storage-tank:0.7797862635611005,
-soccer-ball-field:0.5409846307060925,
-roundabout:0.632179992142947,
-harbor:0.5621019025063557,
-swimming-pool:0.6734955940136754,
-helicopter:0.5240034764561138
+    mAP: 0.6725084704054188
+    ap of each class:
+    plane:0.8928344652701296,
+    baseball-diamond:0.7304554873104306,
+    bridge:0.34861749965443384,
+    ground-track-field:0.6454757711323661,
+    small-vehicle:0.7358328451641203,
+    large-vehicle:0.7333202044070867,
+    ship:0.829537451899966,
+    tennis-court:0.8991725822927377,
+    basketball-court:0.7414258616115545,
+    storage-tank:0.7907933807503915,
+    soccer-ball-field:0.5718970329039421,
+    roundabout:0.5956995083477648,
+    harbor:0.51027563083865,
+    swimming-pool:0.6614448075148596,
+    helicopter:0.4008445269828484
 
 The submitted information is :
 
-Description: RetinaNet_DOTA_1x_20201225_45.9w
+Description: RetinaNet_DOTA_1x_20210610_35.1w
 Username: SJTU-Det
 Institute: SJTU
 Emailadress: yangxue-2019-sjtu@sjtu.edu.cn
 TeamMembers: yangxue
 """
-
 
