cb98435ed853435e31f1d0fddcc3aade55e6611e
Joel Pendleton <JoelPendleton@users.noreply.github.com>
2021-09-11 12:23:22 +0100

    Changed number of steps before saving weights to 10000 so number of total steps is 100k/10 also changed batch size to 10

--- libs/configs/cfgs.py
+++ libs/configs/cfgs.py
@@ -9,11 +9,11 @@ from libs.configs._base_.schedules.schedule_1x import *
 from dataloader.pretrained_weights.pretrain_zoo import PretrainModelZoo
 
 # schedule
-BATCH_SIZE = 1
+BATCH_SIZE = 10
 GPU_GROUP = "0,1,2,3"
 NUM_GPU = len(GPU_GROUP.strip().split(','))
 LR = 0.001 * BATCH_SIZE * NUM_GPU
-SAVE_WEIGHTS_INTE = 4000 * 2
+SAVE_WEIGHTS_INTE = 10000
 DECAY_STEP = np.array(DECAY_EPOCH, np.int32) * SAVE_WEIGHTS_INTE
 MAX_ITERATION = SAVE_WEIGHTS_INTE * MAX_EPOCH
 WARM_SETP = int(WARM_EPOCH * SAVE_WEIGHTS_INTE)
@@ -27,17 +27,12 @@ pretrain_zoo = PretrainModelZoo()
 PRETRAINED_CKPT = pretrain_zoo.pretrain_weight_path(NET_NAME, ROOT_PATH)
 TRAINED_CKPT = os.path.join(ROOT_PATH, 'output/trained_weights')
 
-VERSION = 'FPN_Res50D_QDOT_v1_20210903'
+VERSION = 'FPN_Res50D_QDOT_v1_20210911'
 
 """
 R2CNN
 
-FLOPs: 1235505473;    Trainable params: 41692732
 
-rotation eval:
-Writing diamond VOC resutls file
-cls : diamond|| Recall: 0.9573200992555831 || Precison: 0.8101637967240655|| AP: 0.8907657711228334
-F1:0.9457199070973091 P:0.9445544554455445 R:0.9468982630272953
-mAP is : 0.8907657711228334
+
 
 """
