fc46d0e30deea294fee0e529d285770195505ed1
Joel Pendleton <JoelPendleton@users.noreply.github.com>
2021-09-12 11:48:45 +0100

    Changed batch size I think it may be too big

--- libs/configs/cfgs.py
+++ libs/configs/cfgs.py
@@ -9,7 +9,7 @@ from libs.configs._base_.schedules.schedule_1x import *
 from dataloader.pretrained_weights.pretrain_zoo import PretrainModelZoo
 
 # schedule
-BATCH_SIZE = 100
+BATCH_SIZE = 10
 GPU_GROUP = "0,1,2,3"
 NUM_GPU = len(GPU_GROUP.strip().split(','))
 LR = 0.001 * BATCH_SIZE * NUM_GPU
